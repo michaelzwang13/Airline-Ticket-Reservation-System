cc580e316d23ae9d5aa03862df9c0dd3c1673121
David_Zhan <hz2851@nyu.edu>
2024-11-21 19:50:58 -0500

    change log out and added search flight

--- Part3/project.py
+++ Part3/project.py
@@ -283,6 +283,12 @@ def editStaffProfile():
     cursor.close()
     return render_template('staff_profile.html')
 
+@app.route('/searchFlight',methods=['POST'])
+def searchFlight():
+    for key, value in request.form.items():
+        print(key,value)
+    return render_template('index.html')
+
 @app.route('/customer_flight')
 def customer_flight():
     return render_template('customer_flight.html')
